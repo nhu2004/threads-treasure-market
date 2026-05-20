05ba771b5955fe8c03e822dd52d8e85b83f98a49
Tremmmmm <153413955+Tremmmmm@users.noreply.github.com>
2026-05-20 18:34:01 +0700

    xoa cac file du thua

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
-  { ignores: ["dist", "cleanup-jsx.js", "convert-final.js", "convert-tsx-to-jsx.js"] },
+  { ignores: ["dist", "node_modules"] },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
